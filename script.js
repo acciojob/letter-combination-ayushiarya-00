a2f1db97364dfd359debe1504d5f5d75d3059699
AYUSHI ARYA <83390652+ayushiarya-00@users.noreply.github.com>
2023-01-16 13:06:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+var digitsToLetters = {
+  
+  2: 'abc',
+  3: 'def',
+  4: 'ghi',
+  5: 'jkl',
+  6: 'mno',
+  7: 'pqrs',
+  8: 'tuv',
+  9: 'wxyz'
+  
+};
+
+var letterCombinations = (digits) => {
+    
+  if (digits === '') {
+    return [];
+  }
+
+  var possibleValues = digits.split('').map(value => digitsToLetters[value]);
+  
+  var possibleCombinations = [];
+
+  var recursiveCombine = (start, result) => {
+
+    if (result.length === digits.length) {
+      possibleCombinations.push(result.join(''));
+    }
+
+    for (var i = start; i < possibleValues.length; i++) {
+      for (var j = 0; j < possibleValues[i].length; j++) {
+        result.push(possibleValues[i][j]);
+        recursiveCombine(i + 1, result);
+        result.pop();
+      } 
+    }
+  }
+
+  recursiveCombine(0, []);
+  return possibleCombinations;
+
+};
 
 module.exports = letterCombinations;
